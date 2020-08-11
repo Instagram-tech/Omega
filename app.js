bf44b85c27fb5951b0bf3bdfdc52dc13edfb213d
Instagram-tech <69497495+Instagram-tech@users.noreply.github.com>
2020-08-11 12:44:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCrNVq03--i_F0NfCWGwqHW7_5waNUcR0Y",
+  authDomain: "instagram-e14e6.firebaseapp.com",
+  databaseURL: "https://instagram-e14e6.firebaseio.com",
+  projectId: "instagram-e14e6",
+  storageBucket: "instagram-e14e6.appspot.com",
+  messagingSenderId: "604778604159",
+  appId: "1:604778604159:web:f1af31311c1508d477414a",
+  measurementId: "G-552LRXESRV"
 };
-
      
  
 ////Do Not Edit Below This
